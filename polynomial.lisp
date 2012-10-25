24e578035a91ec604dc6d3479bf034c49fea57f0
chinmay <cdbapat92@gmail.com>
2012-10-25 17:06:53 +0530

    Resolved conflict, adding trignometric

--- polynomial.lisp
+++ polynomial.lisp
@@ -22,7 +22,33 @@
        (make-div (integrate (dividend expr) dvar) (divisor expr))
        )
      )
+    ((isSin expr)
+     (cond 
+       ((eq dvar (cadr expr))(make-prod ('cos dvar) -1))
+       ((isAx (cadr expr)) ())
+           )
+     
+     )
+    ((isCos expr)
+     ('sin )
+     )
     )
+  )
+
+(defun isAx (expr dvar)
+  (and (eq '* (car expr)) 
+       (or 
+         (and (eq dvar (cadr expr)) (not (containsVariable dvar (caddr expr))))
+         (and (eq dvar (caddr expr)) (not (containsVariable dvar (cadr expr))))
+         )
+       )
+  )
+(defun getA (expr dvar)
+  (if ())
+  )
+
+(defun isSin (expr)
+  (eq (car expr) 'sin)
   )
 
 (defun isDiv (expr)
@@ -141,3 +167,13 @@
     )
   )
 
+(defun simplify (expr)
+  (cond 
+    ((isAdd expr)
+     (cond ((eq (simplify (cadr expr)) (simplify (caddr expr))) (list '* (simplify (cadr expr)) 2))
+           ((numberp (simplify (cadr expr)) ))
+           )
+     )
+    )
+  
+  )
